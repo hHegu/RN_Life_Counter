733684087bc57bfc588ff5a8272610636efbd9aa
Henri Helin <henri.helin@nordcloud.com>
2021-06-11 11:43:03 +0300

    feat: keep screen awake

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import Main from './src/views/Main';
-import {Provider} from 'react-redux';
-import React from 'react';
-import {store} from './src/store';
+import Main from './src/views/Main'
+import { Provider } from 'react-redux'
+import React from 'react'
+import { store } from './src/store'
+import { useKeepAwake } from 'expo-keep-awake'
 
 export default function App() {
+  useKeepAwake()
   return (
     <Provider store={store}>
       <Main />
     </Provider>
-  );
+  )
 }
