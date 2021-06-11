346911f461035d36d128d4084b26029fae8278ff
Henri Helin <henri.helin@nordcloud.com>
2021-06-11 12:57:18 +0300

    feat: immersive mode

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
+import React, { useEffect } from 'react'
+
 import Main from './src/views/Main'
 import { Provider } from 'react-redux'
-import React from 'react'
+import { StatusBar } from 'react-native'
 import { store } from './src/store'
 import { useKeepAwake } from 'expo-keep-awake'
 
 export default function App() {
   useKeepAwake()
+
+  useEffect(() => {
+    StatusBar.setHidden(true)
+  }, [])
+
   return (
     <Provider store={store}>
       <Main />
